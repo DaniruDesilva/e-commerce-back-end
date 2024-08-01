dbed0bce005b61592ca0c36ea064ace977733add
Daniru De Silva <165991056+DaniruDesilva@users.noreply.github.com>
2024-08-01 14:39:16 +0530

    E-Commerce-Backend

---
+++ src/infrastructure/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDb = async => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        console.log("Error connecting to the database");
+    }
+}
